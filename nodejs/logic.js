3c3f5b099711141ee1127cee70119cb7881adb23
AlexTavor <alex@imperson.com>
2019-11-26 20:41:27 +0200

    Renamed keyFilename to service_account.json, pulling project id from it

--- nodejs/logic.js
+++ nodejs/logic.js
@@ -1,12 +1,14 @@
 const dialogflow = require("dialogflow");
 const uuid = require("uuid");
 const sdk = require("@aleximp/coco-sdk-nodejs");
+const fs = require("fs");
 
-const projectId = "banking-lskgnd";
+const keyFilename = "./keys/service_account.json";
+const projectId = JSON.parse(fs.readFileSync(keyFilename)).project_id;
 const sessionId = uuid.v4();
 const sessionClient = new dialogflow.SessionsClient({
   projectId,
-  keyFilename: "./keys/banking-lskgnd-b602ad831596.json"
+  keyFilename
 });
 const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
@@ -26,6 +28,7 @@ async function processCoco(input) {
 }
 
 async function process(input) {
+  console.log(`processing, project id is ${projectId}`);
   if (isHandlingAccountOpen) {
     return processCoco(input);
   }
@@ -40,21 +43,29 @@ async function process(input) {
     }
   };
 
-  // Send request and log result
-  const responses = await sessionClient.detectIntent(request);
-  const result = responses[0].queryResult;
-  if (result.intent.displayName === "account.open") {
-    isHandlingAccountOpen = true;
-    return processCoco(input);
+  console.log(`sending to df`);
+  try {
+    const responses = await sessionClient.detectIntent(request);
+    console.log(`df result received`);
+    const result = responses[0].queryResult;
+    if (result.intent.displayName === "account.open") {
+      isHandlingAccountOpen = true;
+      return processCoco(input);
+    }
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
+    if (result.intent) {
+      console.log(`  Intent: ${result.intent.displayName}`);
+    } else {
+      console.log(`  No intent matched.`);
+    }
+    return Promise.resolve(result.fulfillmentText);
+  } catch (e) {
+    console.log(e.message);
+    Promise.resolve(
+      "AN ERROR HAS OCCURRED. WE APOLOGIZE FOR THE INCONVENIENCE"
+    );
   }
-  console.log(`  Query: ${result.queryText}`);
-  console.log(`  Response: ${result.fulfillmentText}`);
-  if (result.intent) {
-    console.log(`  Intent: ${result.intent.displayName}`);
-  } else {
-    console.log(`  No intent matched.`);
-  }
-  return Promise.resolve(result.fulfillmentText);
 }
 
 exports.process = process;
